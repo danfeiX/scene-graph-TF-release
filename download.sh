4c9e3c6a5cb0e6a241a92dc9b786f74e69ca35c4
danfei <danfeiX@users.noreply.github.com>
2018-09-30 20:25:44 -0700

    Update download link

--- download.sh
+++ download.sh
@@ -3,13 +3,13 @@
 mkdir -p data/
 
 echo 'Downloading the mini-vg dataset...'
-wget http://svl.stanford.edu/scene-graph/dataset/mini-vg.zip
+wget http://svl.stanford.edu/projects/scene-graph/dataset/mini-vg.zip
 unzip mini-vg.zip
 mv vg data/
 rm mini-vg.zip
 
 echo 'Downloading the final model checkpoint...'
-wget http://svl.stanford.edu/scene-graph/checkpoints/dual_graph_vrd_final_iter2_checkpoint.zip
+wget http://svl.stanford.edu/projects/scene-graph/checkpoints/dual_graph_vrd_final_iter2_checkpoint.zip
 unzip dual_graph_vrd_final_iter2_checkpoint.zip
 mv dual_graph_vrd_final_iter2 checkpoints
 rm dual_graph_vrd_final_iter2_checkpoint.zip
